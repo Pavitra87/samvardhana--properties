63f103afbcb9f94f4220dd401a9bf34a78fb80de
Pavitra <pavitramn2000@gmail.com>
2025-05-22 19:38:39 +0530

    server.j

--- api/server.js
+++ api/server.js
@@ -25,36 +25,36 @@ app.use('/adminpanel', express.static(path.join(__dirname, 'adminpanel/build')))
 
 
 //projectroutes
-app.use('/api/project',require('./routes/projectRoutes/projectRoutes'))
-app.use('/api/featureproject',require('./routes/projectRoutes/featureProjectRoutes'))
-app.use('/api/keyanimate',require('./routes/projectRoutes/keyAnimatesRoutes'))
-app.use('/api/masterplan',require('./routes/projectRoutes/masterplanRoutes'))
-app.use('/api/locationhigh',require('./routes/projectRoutes/locationHighlightRoutes'))
+// app.use('/api/project',require('./routes/projectRoutes/projectRoutes'))
+// app.use('/api/featureproject',require('./routes/projectRoutes/featureProjectRoutes'))
+// app.use('/api/keyanimate',require('./routes/projectRoutes/keyAnimatesRoutes'))
+// app.use('/api/masterplan',require('./routes/projectRoutes/masterplanRoutes'))
+// app.use('/api/locationhigh',require('./routes/projectRoutes/locationHighlightRoutes'))
 
 //aboutroutes
-app.use('/api/about',require('./routes/aboutRoutes/aboutRoutes'))
-app.use('/api/choice',require('./routes/aboutRoutes/chooseRoutes'))
-app.use('/api/offer',require('./routes/aboutRoutes/offerRoutes'))
-app.use('/api/journey',require('./routes/aboutRoutes/ourjourneyRoutes'))
+// app.use('/api/about',require('./routes/aboutRoutes/aboutRoutes'))
+// app.use('/api/choice',require('./routes/aboutRoutes/chooseRoutes'))
+// app.use('/api/offer',require('./routes/aboutRoutes/offerRoutes'))
+// app.use('/api/journey',require('./routes/aboutRoutes/ourjourneyRoutes'))
 
 //bannerroutes
-app.use('/api/aboutbanner',require('./routes/bannerRoutes/aboutBannerRoutes'))
-app.use('/api/blogbanner',require('./routes/bannerRoutes/blogBannerRoutes'))
-app.use('/api/contactbanner',require('./routes/bannerRoutes/contactBannerRoutes'))
-app.use('/api/homebanner',require('./routes/bannerRoutes/homeBannerRoutes'))
-app.use('/api/projectbanner',require('./routes/bannerRoutes/projectBannerRoutes'))
+// app.use('/api/aboutbanner',require('./routes/bannerRoutes/aboutBannerRoutes'))
+// app.use('/api/blogbanner',require('./routes/bannerRoutes/blogBannerRoutes'))
+// app.use('/api/contactbanner',require('./routes/bannerRoutes/contactBannerRoutes'))
+// app.use('/api/homebanner',require('./routes/bannerRoutes/homeBannerRoutes'))
+// app.use('/api/projectbanner',require('./routes/bannerRoutes/projectBannerRoutes'))
 
 //blogroutes
 app.use('/api/blog',require('./routes/blogRoutes/blogRoutes'))
 
 //customerroutes
-app.use('/api/customer',require('./routes/customeRoutes/customerRoute'))
+// app.use('/api/customer',require('./routes/customeRoutes/customerRoute'))
 
 //feqroutes
 app.use('/api/faqs',require('./routes/faqRoutes/faqRoutes'))
 
 //serviceroutes
-app.use('/api/service',require('./routes/serviceRoutes/serviceRoutes'))
+// app.use('/api/service',require('./routes/serviceRoutes/serviceRoutes'))
 
 app.get('/adminpanel/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'adminpanel/build', 'index.html'));
