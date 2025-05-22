9fbf0ecd9209e0ee90f28ed33c022844e425e347
Pavitra <pavitramn2000@gmail.com>
2025-05-22 19:05:15 +0530

    app.js

--- admin-panel/src/App.jsx
+++ admin-panel/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route, BrowserRouter, Outlet } from "react-router-dom"; // ✅ All at top
+import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom"; // ✅ All at top
 import DashboardHeader from "./component/DashboardHeader";
 
 import CreateAboutPage from "./Dashboard/about/CreateAboutPage";
@@ -12,23 +12,24 @@ import FaqDashboard from "./Dashboard/faqs/FaqDashboard";
 import CreateFaqForm from "./Dashboard/faqs/CreateFaqForm";
 import EditFaq from "./Dashboard/faqs/EditFaq";
 
-
 const App = () => {
   return (
     <BrowserRouter>
-    <DashboardHeader/>
+      <DashboardHeader />
       <Routes>
         <Route path="/" element={<AdminDashboard />}>
           {/* <Route path="createabout" element={<CreateAboutPage />} /> */}
           {/* <Route path="aboutdashboard" element={<AboutDashboard />} /> */}
+          <Route index element={<Navigate to="blogdashboard" replace />} />
           <Route path="blogdashboard" element={<BlogDashboard />} />
           <Route path="createblog" element={<CreateBlogForm />} />
           <Route path="editblog/:id" element={<EditBlog />} />
           <Route path="faqdashboard" element={<FaqDashboard />} />
           <Route path="createfaq" element={<CreateFaqForm />} />
           <Route path="editfaq/:id" element={<EditFaq />} />
+
+          <Route path="*" element={<Navigate to="blogdashboard" replace />} />
         </Route>
-        
       </Routes>
     </BrowserRouter>
   );
