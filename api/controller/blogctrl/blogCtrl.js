295718bf2ce6040b0f209a59316c20b309b32981
Pavitra <pavitramn2000@gmail.com>
2025-05-24 10:25:55 +0530

    edit project

--- api/controller/blogctrl/blogCtrl.js
+++ api/controller/blogctrl/blogCtrl.js
@@ -60,32 +60,34 @@ exports.getById = async (req, res) => {
 
 // Update
 exports.update = async (req, res) => {
-  if (!req.body) {
-    return res.status(400).json({ error: "Missing request body" });
-  }
-
-  const { id } = req.params;
-  const { name, heading, description, message, imgUrl, profilepic } = req.body;
-
-  try {
-    const updatedBlog = await Blog.findByIdAndUpdate(
-      id,
-      { name, heading, description, message, imgUrl, profilepic },
-      { new: true }
-    );
-
-    if (!updatedBlog) {
-      return res.status(404).json({ message: "Blog not found" });
+   const { id } = req.params;
+    const { heading, description,name } = req.body;
+  
+    let imgUrl = req.body.imgUrl;
+    if (req.file) {
+      imgUrl = req.file.filename; // or path if you need full URL
     }
-
-  res.status(200).json({
-    success:true,
-  message: "Blog updated successfully",
-  data: updatedBlog,
-});
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
+  
+    try {
+      const updateData = { heading, description,name };
+      if (imgUrl) updateData.imgUrl = imgUrl;
+  
+      const updatedBlog = await Blog.findByIdAndUpdate(id, updateData, {
+        new: true,
+      });
+  
+      if (!updatedBlog) {
+        return res.status(404).json({ message: "not found" });
+      }
+  
+      res.status(200).json({
+        success: true,
+        message: " updated successfully",
+        data: updatedBlog,
+      });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
 };
 
 // Delete
